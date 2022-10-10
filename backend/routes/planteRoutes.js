3e449128645e938b3aadeb9f5a0fdd6f55af68ca
Bastien <45488913+BastienBiblocque@users.noreply.github.com>
2022-10-10 09:21:53 +0200

    middleware d'auth

--- backend/routes/planteRoutes.js
+++ backend/routes/planteRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const planteCtrl = require('../controllers/plante')
+
+const planteCtrl = require('../controllers/plante');
+
 const auth = require('../middleware/auth');
 
-router.get('/', auth, planteCtrl.list)
+router.get('/', auth, planteCtrl.list);
 
 module.exports = router;

1a9a55a22b61bdbacc80c791090310a75316cdf4
Bastien <45488913+BastienBiblocque@users.noreply.github.com>
2022-10-14 00:15:13 +0200

    Crud plante + route

--- backend/Controllers/plante.js
+++ backend/Controllers/plante.js
@@ -5,3 +5,24 @@ exports.list = (req,res) =>{
         res.status(200).json(plantes)
     }).catch(error=> res.status(400).json({error}))
 }
+
+exports.create = (req,res) =>{
+    const plante = new Plante({
+        ...req.body
+    })
+    plante.save().then(()=>{
+        res.status(201).json({message:'Plante enregistrée'})
+    }).catch(error=> res.status(400).json({error}))
+}
+
+exports.delete = (req,res) =>{
+    Plante.deleteOne({_id:req.params.id}).then(()=>{
+        res.status(200).json({message:'Plante supprimée'})
+    }).catch(error=> res.status(400).json({error}))
+}
+
+exports.update = (req,res) =>{
+    Plante.updateOne({_id:req.params.id}, {...req.body, _id:req.params.id}).then(()=>{
+        res.status(200).json({message:'Plante modifiée'})
+    }).catch(error=> res.status(400).json({error}))
+}
--- backend/routes/planteRoutes.js
+++ backend/routes/planteRoutes.js
@@ -6,5 +6,9 @@ const planteCtrl = require('../controllers/plante');
 const auth = require('../middleware/auth');
 
 router.get('/', auth, planteCtrl.list);
+router.post('/', auth, planteCtrl.create);
+router.delete('/:id', auth, planteCtrl.delete);
+router.put('/:id', auth, planteCtrl.update);
+
 
 module.exports = router;

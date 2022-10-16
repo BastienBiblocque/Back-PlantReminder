d370805a986dbaf29fbc02c54b16987492407d48
Bastien <45488913+BastienBiblocque@users.noreply.github.com>
2022-10-16 16:53:45 +0200

    Modifier son profil

--- backend/Utils/getJwt.js
+++ backend/Utils/getJwt.js
@@ -2,6 +2,5 @@ const jwt = require("jsonwebtoken");
 
 exports.getUserIdWithJwt = (req) =>{
     const decodedToken = jwt.verify(req.headers.authorization.split(' ')[1], 'RANDOM_TOKEN_SECRET');
-    console.log(decodedToken.userId);
     return decodedToken.userId;
 }
